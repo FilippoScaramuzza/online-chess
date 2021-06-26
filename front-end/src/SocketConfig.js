7d91b0509929bc43efd15018ecebb333f5ae3914
Filippo Scaramuzza <filippo@Air-di-Filippo.lan>
2021-06-26 17:09:22 +0200

    update code for deploy

--- front-end/src/SocketConfig.js
+++ front-end/src/SocketConfig.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket("https://vast-woodland-86915.herokuapp.com/");
+const socket = openSocket("https://vast-woodland-86915.herokuapp.com/", {transports:['websocket']});
 
 export default socket;
