bdd581f3b6ff9c66e64de0f789ae7a7135daba8c
Filippo Scaramuzza <filippo@MacBook-Air-di-Filippo.local>
2021-07-14 11:19:34 +0200

    moved to online server

--- front-end/src/SocketConfig.js
+++ front-end/src/SocketConfig.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client';
 
-//const socket = openSocket("https://fs-chess-backend.herokuapp.com", {transports:['websocket']});
-const socket = openSocket("http://192.168.1.86:4000", {transports:['websocket']});
+const socket = openSocket("https://fs-chess-backend.herokuapp.com", {transports:['websocket']});
+//const socket = openSocket("http://192.168.1.86:4000", {transports:['websocket']});
 
 export default socket;
