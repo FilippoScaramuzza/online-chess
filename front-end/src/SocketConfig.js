f114ae4c6f906b7aa007426b57266212af09318e
Filippo Scaramuzza <filippo@Air-di-Filippo.lan>
2021-07-22 22:21:13 +0200

    cors fix? 2

--- front-end/src/SocketConfig.js
+++ front-end/src/SocketConfig.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket("https://testpythonserver.herokuapp.com", transports={['websocket']})
+const socket = openSocket("https://testpythonserver.herokuapp.com", {transports: ['websocket']})
 //const socket = openSocket("http://localhost:8080")
 
 export default socket;
