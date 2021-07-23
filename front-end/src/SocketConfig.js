73165ee22468c89f09f3649c9cad3d62f51b0df5
Filippo Scaramuzza <filippo@Air-di-Filippo.lan>
2021-07-23 15:08:05 +0200

    Finished migration

--- front-end/src/SocketConfig.js
+++ front-end/src/SocketConfig.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket("https://testpythonserver.herokuapp.com")
+const socket = openSocket("https://onlinechess-py-backend.herokuapp.com")
 //const socket = openSocket("http://localhost:8080")
 
 export default socket;
