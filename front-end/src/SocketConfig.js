83b2bf07ceebaad0ae6f03a9569460ad4942e1f8
Filippo Scaramuzza <filippo.scaramuzza1999@gmail.com>
2023-04-11 18:04:26 +0200

    changing socket config to render

--- front-end/src/SocketConfig.js
+++ front-end/src/SocketConfig.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket("https://onlinechesspybackend-filipposcaramuzza1999.b4a.run/", {transports: ['websocket']})
+const socket = openSocket("https://onlinechess-py-backend.onrender.com", {transports: ['websocket']})
 //const socket = openSocket("http://localhost:8080")
 
 export default socket;
